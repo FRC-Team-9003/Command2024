0b1b8f9832b78c822a8f965d71b8095ea33ca742
a-rodriquez <157540478+a-rodriquez@users.noreply.github.com>
2024-01-29 17:15:17 -0500

    made subsystem template

---
+++ src/main/java/frc/robot/subsystems/Template.java
@@ -0,0 +1,16 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Template extends SubsystemBase{
+    @Override
+    public void periodic(){
+
+    }
+
+
+    @Override public void simulationPeriodic(){
+        
+    }
+}
