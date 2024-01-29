a9db474ee07b57a4d43aa4bccbd4d1d883ea1a74
a-rodriquez <157540478+a-rodriquez@users.noreply.github.com>
2024-01-29 18:00:43 -0500

    Updated Subsytem Files

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,9 +1,19 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ElevatorConstants;
 
 public class Elevator extends SubsystemBase{
+
+    private CANSparkMax elev;
+
+    public Elevator(){
+        elev = new CANSparkMax(ElevatorConstants.kElevatorCanID, MotorType.kBrushless);
+    }
+
     @Override
     public void periodic(){
 
--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,9 +1,22 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ShooterConstants;
 
 public class Shooter extends SubsystemBase{
+   
+    private CANSparkMax shoota;
+    private CANSparkMax shootb;
+
+    public Shooter(){
+        shoota = new CANSparkMax(ShooterConstants.NeoBottom, MotorType.kBrushless);
+        shootb = new CANSparkMax(ShooterConstants.NeoTop, MotorType.kBrushless);
+
+    }
+   
     @Override
     public void periodic(){
 
