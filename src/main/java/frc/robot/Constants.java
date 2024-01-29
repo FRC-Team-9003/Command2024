6c8ff24d0a8e005ef73cd1de54176676b2fbb956
a-rodriquez <157540478+a-rodriquez@users.noreply.github.com>
2024-01-29 16:45:27 -0500

    updated canIDs

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -139,4 +139,9 @@ public final class Constants {
   public static final class NeoMotorConstants {
     public static final double kFreeSpeedRpm = 5676;
   }
+
+
+  public static final class ElevatorConstants{
+    public static final int kElevatorCanID= 20;
+  }
 }
