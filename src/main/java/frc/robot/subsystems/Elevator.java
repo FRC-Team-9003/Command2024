2f72a468c97881b14cdec5aeab2e1d63bfcbd4d8
a-rodriquez <157540478+a-rodriquez@users.noreply.github.com>
2024-01-30 16:22:33 -0500

    added absolute encoders

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,16 +1,21 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.SparkAbsoluteEncoder.Type;
+import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkAbsoluteEncoder;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ElevatorConstants;
 
 public class Elevator extends SubsystemBase {
 
   private CANSparkMax elev;
-
+  private AbsoluteEncoder elevEncoder;
   public Elevator() {
     elev = new CANSparkMax(ElevatorConstants.kElevatorCanID, MotorType.kBrushless);
+    elevEncoder = elev.getAbsoluteEncoder(Type.kDutyCycle);
   }
 
   @Override
